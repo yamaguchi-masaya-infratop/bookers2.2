16f0d394d2d197eecaf6431c7958cd42a3c2b121
EC2 Default User <ec2-user@ip-172-31-4-234.ap-northeast-1.compute.internal>
2023-04-26 09:30:28 +0000

    [Error]Update Error not showing

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,11 +28,11 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
     else
-      @book = Book.find(params[:id])
+       @book = Book.find(params[:id])
       render :edit
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   end
   
   def update
-    user = User.find(params[:id])
-    if user.update(user_params)
-      redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
     else
       @user = User.find(params[:id])
       render :edit
